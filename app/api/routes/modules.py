dd35f1398be22673c6e9b35c2597a87f44f14694
Irving Jared Escalante Sianuqui <irvingsianuqui@gmail.com>
2025-12-26 21:27:50 -0700

    Eliminar modulo ya hecho

--- app/api/routes/modules.py
+++ app/api/routes/modules.py
@@ -19,7 +19,7 @@ router = APIRouter()
 
 @router.get("/getAll/{id_course}", response_model=List[ModuleCourseResponse])
 def get_modules(id_course:int, db:Session = Depends(get_db)):
-    module_course = db.query(ModuleCourse).filter(ModuleCourse.id_course == id_course).order_by(ModuleCourse.order_index.asc()).all()
+    module_course = db.query(ModuleCourse).filter(ModuleCourse.id_course == id_course, ModuleCourse.status_module == 1).order_by(ModuleCourse.order_index.asc()).all()
     if not module_course:
         raise HTTPException(status_code=404, detail="No se tiene modulos este curso")
     return module_course
@@ -66,6 +66,20 @@ def edit_module(id_module:int, module:EditModule, db:Session = Depends(get_db), 
         existing_module.status_module = module.status_module
     if module.order_index is not None:
         existing_module.order_index = module.order_index
+    db.commit()
+    db.refresh(existing_module)
+    return existing_module
+
+@router.patch("/delete/{id_module}", response_model=ModuleCourseResponse)
+def delete_module(id_module:int, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    existing_module = db.query(ModuleCourse).filter(ModuleCourse.id_module == id_module).first()
+    if not existing_module:
+        raise HTTPException(status_code=404, detail="Module not found")
+    if not db.query(Course).filter(Course.id_course == existing_module.id_course, Course.id_user == current_user.id).first():
+        raise HTTPException(status_code=403, detail="Forbidden")
+    existing_module.status_module = 0
     db.commit()
     db.refresh(existing_module)
     return existing_module
