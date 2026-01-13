4ed8a0f0c5aa152c7ebfdfed245616d74c7b1f17
Irving Jared Escalante Sianuqui <irvingsianuqui@gmail.com>
2026-01-13 12:23:16 -0700

    Se le aumento a 200 el nombre de las publis

--- app/models/course_publish.py
+++ app/models/course_publish.py
@@ -10,7 +10,7 @@ class CoursePublish(Base):
     id_course_publish = Column(Integer, primary_key=True, autoincrement=True, index=True)
     uuid_publish = Column(String(36), default=lambda: str(uuid.uuid4()), nullable=False)
     id_module = Column(Integer, ForeignKey("module_course.id_module"))
-    name_publication = Column(String(50), nullable=False)
+    name_publication = Column(String(200), nullable=False)
     description = Column(String(500), nullable=False)
     date_created = Column(DateTime, default=datetime.now)
     date_updated = Column(DateTime, nullable=True)
